ed6baaa99247af1d43ba6d6a6dab12bc05473cc2
James Blackman <JBlack262@west-mec.org>
2021-04-01 16:08:46 -0700

    first commit

---
+++ client/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Navbar title="Custom Laser Engraving and Cutting"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
